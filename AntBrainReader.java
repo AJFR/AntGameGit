b849f0d94934ce528978c1080fdf7b968bd61c07
Farmerman <farmerman.ar@gmail.com>
2016-05-04 13:29:53 +0100

    Update AntBrainReader.java

--- AntBrainReader.java
+++ AntBrainReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ant.brain.reader;
+package antgame;
 
 import java.io.File;
 import java.io.FileNotFoundException;
