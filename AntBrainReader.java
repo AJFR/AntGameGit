7e34203626b896e3132cc0bd72a2099604ab2089
mattnifico <mattmurphy2183@gmail.com>
2016-05-04 16:47:34 +0100

    Added files via upload

--- AntBrainReader.java
+++ AntBrainReader.java
@@ -1,33 +1,54 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package antgame;
+package assembledantgame;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  *
- * @author ajfr20
+ * @author mfm29
  */
 public class AntBrainReader {
 
+    private ArrayList<String> FSA;
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
+
+    /**
+     *
+     * @author ajfr20
+     */
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
-        
-        Scanner sc = new Scanner(new File("brain.txt"));
-        while (sc.hasNextLine()) {
-            //String[] command = sc.nextLine().split(" ");
-            //for(String s: command){
-            //System.out.println(s);
-            String command = sc.nextLine();
-            System.out.println(command);
+    public AntBrainReader()  {
+        FSA = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File("AntBrainFSA..txt")); //Replace with File name if you can get the browser working
+
+            int i = 0;
+            while (sc.hasNextLine()) {
+                //String[] command = sc.nextLine().split(" ");
+                //for(String s: command){
+                //System.out.println(s);
+                String command = sc.nextLine();
+                FSA.add(command);
+                i++;
             }
-      }  
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
     }
-   
+
+    public ArrayList<String> getFSA() {
+        return FSA;
+    }
+}
