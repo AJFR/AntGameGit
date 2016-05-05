2b2da5d5021f4a529b38dd0b651e59654482d37b
Farmerman <farmerman.ar@gmail.com>
2016-05-05 12:56:07 +0100

    DO NOT USE

--- AntGame.java
+++ AntGame.java
@@ -3,6 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
+ //DO NOT USE OLD INTERFACE
+ 
+ 
 package antgame;
 import java.awt.EventQueue;
 
