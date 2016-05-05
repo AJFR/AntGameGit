c50a04e1db172882ce048500891bbd8488e1948e
Feroze1 <hulk_foggy11@hotmail.com>
2016-05-05 22:19:47 +0100

    Added files via upload

---
+++ New_FileChooserTest1.java
@@ -0,0 +1,72 @@
+package filechoosertest;
+
+//
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileChooserTest extends JFrame {
+  private JTextField filename = new JTextField(), dir = new JTextField();
+  public String pathName;
+  private JButton open = new JButton("Open"), save = new JButton("Save");
+
+  public FileChooserTest() {
+    JPanel p = new JPanel();
+    open.addActionListener(new OpenL());
+    p.add(open);
+    save.addActionListener(new SaveL());
+    p.add(save);
+    Container cp = getContentPane();
+    cp.add(p, BorderLayout.SOUTH);
+    dir.setEditable(false);
+    filename.setEditable(false);
+    p = new JPanel();
+    p.setLayout(new GridLayout(2, 1));
+    p.add(filename);
+    p.add(dir);
+    cp.add(p, BorderLayout.NORTH);
+  }
+
+  class OpenL implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+      JFileChooser c = new JFileChooser();
+      c.addChoosableFileFilter(new FileNameExtensionFilter("Text Files","brain"));
+      // Demonstrate "Open" dialog:
+      int rVal = c.showOpenDialog(FileChooserTest.this);
+      if (rVal == JFileChooser.APPROVE_OPTION) {
+        filename.setText(c.getSelectedFile().getName());
+        dir.setText(c.getCurrentDirectory().toString());
+        pathName = c.getCurrentDirectory().toString();
+        System.out.println(pathName);
+      }
+      if (rVal == JFileChooser.CANCEL_OPTION) {
+        filename.setText("You pressed cancel");
+        dir.setText("");
+      }
+    }
+  }
+
+  class SaveL implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+       //pathName = EngineString(to ininstall the brain);
+      }
+    }
+  
+ public static void FileChooserTest() {
+    
+  }
+  public static void run(JFrame frame, int width, int height) {
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(width, height);
+    frame.setVisible(true);
+  }
+}
